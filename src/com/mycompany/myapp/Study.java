57c233893fbd3d4ee534db903089fc8572b8c44e
David Hurtado <davidjhurtado@gmail.com>
2015-09-20 09:21:29 -0430

    Estudios y Temas Bibliclos
    Entidades de studios y temas

---
+++ src/com/mycompany/myapp/Study.java
@@ -0,0 +1,42 @@
+package com.mycompany.myapp;
+
+/**
+ * Created by David on 14/09/2015.
+ */
+public class Study {
+    private int idStudy = 0;
+    private String Title = "";
+    private double Percent = 0;
+
+    public int getIdStudy() {
+        return idStudy;
+    }
+
+    public void setIdStudy(int idStudy) {
+        this.idStudy = idStudy;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public double getPercent() {
+        return Percent;
+    }
+
+    public void setPercent(double percent) {
+        Percent = percent;
+    }
+    public Study (){
+    }
+    public Study(int idStudy, String title, double percent) {
+        super();
+        this.idStudy = idStudy;
+        Title = title;
+        Percent = percent;
+    }
+}
---
+++ src/com/mycompany/myapp/Theme.java
@@ -0,0 +1,93 @@
+package com.mycompany.myapp;
+
+/**
+ * Created by David on 14/09/2015.
+ */
+public class Theme {
+    private int idStudy = 0;
+    private  int idTheme = 0;
+    private String Title = "";
+    private String HTMLContent = "";
+    private String OPTION1 = "";
+    private String OPTION2 = "";
+    private String OPTION3 = "";
+    private String ANSWER = "";
+
+    public int getIdStudy() {
+        return idStudy;
+    }
+
+    public void setIdStudy(int idStudy) {
+        this.idStudy = idStudy;
+    }
+
+    public int getIdTheme() {
+        return idTheme;
+    }
+
+    public void setIdTheme(int idTheme) {
+        this.idTheme = idTheme;
+    }
+
+    public String getHTMLContent() {
+        return HTMLContent;
+    }
+
+    public void setHTMLContent(String HTMLContent) {
+        this.HTMLContent = HTMLContent;
+    }
+
+    public String getOPTION1() {
+        return OPTION1;
+    }
+
+    public void setOPTION1(String OPTION1) {
+        this.OPTION1 = OPTION1;
+    }
+
+    public String getOPTION2() {
+        return OPTION2;
+    }
+
+    public void setOPTION2(String OPTION2) {
+        this.OPTION2 = OPTION2;
+    }
+
+    public String getOPTION3() {
+        return OPTION3;
+    }
+
+    public void setOPTION3(String OPTION3) {
+        this.OPTION3 = OPTION3;
+    }
+
+    public String getANSWER() {
+        return ANSWER;
+    }
+
+    public void setANSWER(String ANSWER) {
+        this.ANSWER = ANSWER;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public Theme() {
+        super();
+    }
+    public Theme(int idTheme, int idStudy, String Title, String HTMLContent, String OPTION1, String OPTION2, String OPTION3, String ANSWER ){
+        this.idTheme = idTheme;
+        this.idStudy = idStudy;
+        this.Title = Title;
+        this.HTMLContent = HTMLContent;
+        this.OPTION1 = OPTION1;
+        this.OPTION2 = OPTION2;
+        this.OPTION3 = OPTION3;
+        this.ANSWER = ANSWER;
+    }
+}
