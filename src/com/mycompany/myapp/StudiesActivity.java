ce1f69bd0109ba255f48c04e1f3fe41bc83dcb1c
David Hurtado <davidjhurtado@gmail.com>
2015-09-20 09:20:00 -0430

    Estudios
    Modificación para que muestre de manera correcta la lista de estudios
    disponibles

--- src/com/mycompany/myapp/StudiesActivity.java
+++ src/com/mycompany/myapp/StudiesActivity.java
@@ -1,11 +1,10 @@
 package com.mycompany.myapp;
 
 import android.app.*;
+import android.content.Intent;
 import android.os.*;
-import android.view.*;
+import android.view.View;
 import android.widget.*;
-import android.content.*;
-import junit.framework.*;
 
 public class StudiesActivity extends Activity
 {
@@ -15,34 +14,37 @@ public class StudiesActivity extends Activity
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.studies);
-		  // Get ListView object from xml
-            listView = (ListView) findViewById(R.id.lstStudies);
-            
-            // Defined Array values to show in ListView
-            String[] values = new String[] { "Estudio 1", 
+		// Get ListView object from xml
+        listView = (ListView) findViewById(R.id.lstStudies);
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Estudio 1",
                                              "Estudio 2",
                                              "Estudio 3",
-                                             "Estudio 4", 
+                                             "Estudio 4",
                                              "Estudio 5"
                                             };
-    
+
 	 // Define a new Adapter
             // First parameter - Context
             // Second parameter - Layout for the row
             // Third parameter - ID of the TextView to which the data is written
             // Forth - the Array of data
-    
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-              android.R.layout.simple_list_item_1, android.R.id.text1, values);
-    
-    
-            // Assign adapter to ListView
-            listView.setAdapter(adapter); 
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.studiesitem, values);
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Item " + listView.getItemAtPosition(position), Toast.LENGTH_LONG).show();
+                Intent activity = new Intent(StudiesActivity.this, StudiesInteractionActivity.class);
+                startActivity(activity);
+
+            }
+        });
+
     }
 
-   /* @Override
-    protected void onListItemClick(ListView lv, View v, int position, long id){
-        String selectedValue = (String) getListAdapter().getItem(position);
-        Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
-    }*/
 }
