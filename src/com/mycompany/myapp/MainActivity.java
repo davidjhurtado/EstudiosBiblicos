173a89f038c1de3ed47258f9904aeca62c7dc3ce
David Hurtado <davidjhurtado@gmail.com>
2016-01-16 05:05:47 -0430

    Cambio al enviar mensaje
    No envia el mensaje cuando no tiene registros

--- src/com/mycompany/myapp/MainActivity.java
+++ src/com/mycompany/myapp/MainActivity.java
@@ -21,8 +21,8 @@ public class MainActivity extends Activity
         List users = db.getAllUsers();
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
         if (users.size() > 0){
-            alert.setMessage("Size list: " + String.valueOf( users.size()) );
-            alert.show();
+            /*alert.setMessage("Size list: " + String.valueOf( users.size()) );
+            alert.show();*/
         } else {
             alert.setMessage("Size Cero : " + String.valueOf( users.size()) );
             alert.show();
@@ -78,5 +78,38 @@ public class MainActivity extends Activity
 			test.setText(e.toString());
 			//System.out.println(e.toString());
 		}
+	}
+
+	public void onClickmyButton (View v) {
+		final GMailSender sender = new GMailSender("dagsapp@gmail.com", "appdags.123456");
+		new AsyncTask<Void, Void, Void>() {
+			@Override
+			public Void doInBackground(Void... arg) {
+				try {
+
+
+					GMailSender sender = new GMailSender("dagsapp@gmail.com", "appdags.123456");
+					sender.sendMail("This is Subject",
+							"This is Body",
+							"dagsapp@gmail.com",
+							"davidjhurtado@gmail.com");
+
+			/*
+            Mail mail = new Mail("dagsapp@gmail.com", "appdags.123456");
+			String[] toArr = {"davidjhurtado@gmail.com", "dagsapp@gmail.com"};
+			mail.setTo(toArr);
+			mail.setFrom("dagsapp@gmail.com");
+			mail.setSubject("Test App Estudios Biblicos");
+            mail.setBody("Esto es un test");
+            if (mail.send()) {
+
+            }*/
+				} catch (Exception vEx) {
+					//throw new RuntimeException(vEx.getMessage());
+					Toast.makeText(getApplicationContext(), vEx.getMessage(), Toast.LENGTH_LONG).show();
+				}
+			return null;}
+		}.execute();
+		Toast.makeText(getApplicationContext(), "Completado...", Toast.LENGTH_LONG).show();
 	}
 }
